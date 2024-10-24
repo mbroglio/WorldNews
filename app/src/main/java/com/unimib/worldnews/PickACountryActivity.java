f2a2baf07f7cf997a6ea64b1af52131492eae5b6
narger <52449688+narger-ef@users.noreply.github.com>
2024-10-24 18:36:31 +0200

    Esercitazione 3 (Fix)

---
+++ app/src/main/java/com/unimib/worldnews/PickACountryActivity.java
@@ -0,0 +1,15 @@
+package com.unimib.worldnews;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PickACountryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_a_country);
+
+    }
+}
